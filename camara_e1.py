f34f5621b9b927d55f8a054d4a5b5aae708dbfdc
boboycamara <johnismaelc9@gmail.com>
2018-10-26 00:32:39 +0800

    My First Excercise

---
+++ camara_e1.py
@@ -0,0 +1,11 @@
+data = { "Name" : "John Ismael",
+"Spirit Animal" : "Tiger",
+"Reason" : "Intelegent",
+"Hobby" : "Table-tennis",
+"Profession" : "Software Ingineer"}
+
+print("I am {}".format(data["Name"]))
+print("My spirit animal is {}".format(data["Spirit Animal"]))
+print("because {}".format(data["Reason"]))
+print("When not in school, I love t {}".format(data["Hobby"]))
+print("I dream of being a/an {}".format(data["Profession"]))
