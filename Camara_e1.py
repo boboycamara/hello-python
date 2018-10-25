9091aa29283ae3eede1da1e72c79853c32868e0c
boboycamara <johnismaelc9@gmail.com>
2018-10-25 23:56:40 +0800

    Excercise No.1

---
+++ Camara_e1.py
@@ -0,0 +1,13 @@
+data = {"Name" : "John Ismael B. Camara",
+        "Spirit Animal" : "Tiger",
+        "Reason" : "Strong",
+        "Hobby" : "Playing Table-tennis",
+        "Profession" : "Software Ingineer"
+ } 
+
+
+print("I am{}".format(data ["Coplete Name"]))
+print("My spirit animal is{}".format(data ["Your Spirit Animal"]))
+print("because {}".format(data ["Your Reason Why You Think It Represents You"]))
+print("When not in school, I love to{}".format(data ["Hobby"]))
+print("I dream of being a/an {}".format(data ["Profession"]))
---
+++ python-datatype.py
@@ -0,0 +1,11 @@
+age = 16
+weight = 75.45
+
+data = {"name" : "Ismael",
+     "age" : 16,
+    "weight" :78.45
+      }
+#print (data)
+print("I am {}".format(data["name"]))
+print("My first FIL grade is {}"
+    .format(data["grades"][0]))
